199edbb00c66bbbce98f2957b9e3cea1d5920785
32Valik92 <valokstrukalo@gmail.com>
2023-06-11 11:45:00 +0300

    Lesson 5 fixed

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -2,7 +2,7 @@ import { User } from "../models/User.model";
 import { IUser } from "../types/user.types";
 
 class UserRepository {
-  public async create(data: IUser): Promise<IUser> {
+  public async create(data: IUser): Promise<IUser | any> {
     return User.create(data);
   }
 }
